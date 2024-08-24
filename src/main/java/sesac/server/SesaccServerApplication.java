6d34937e0185030729739f3655c7cf1aafc36f69
rtg <rtg1539@naver.com>
2024-08-24 21:15:16 +0900

    initial commit

---
+++ src/main/java/sesac/server/SesaccServerApplication.java
@@ -0,0 +1,13 @@
+package sesac.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesaccServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SesaccServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sesac/server/SesaccServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sesac.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SesaccServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
