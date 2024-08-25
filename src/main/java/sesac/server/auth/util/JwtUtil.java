5b7e96aab0727d3a671dd7de8cfcf30da04e531e
rtg <rtg1539@naver.com>
2024-08-25 15:21:25 +0900

    feat: JwtUtil

---
+++ src/main/java/sesac/server/auth/util/JwtUtil.java
@@ -0,0 +1,55 @@
+package sesac.server.auth.util;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Log4j2
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String key;
+
+    public String generateToken(Map<String, Object> valueMap, int days) {
+
+        log.info("generateKey..." + key);
+
+        // 헤더 부분
+        Map<String, Object> headers = new HashMap<>();
+        headers.put("typ", "JWT");
+        headers.put("alg", "HS256");
+
+        // payload 부분 설정
+        Map<String, Object> payloads = new HashMap<>();
+        payloads.putAll(valueMap);
+
+        // 테스트 시에는 짧은 유효 기간
+        int time = (60 * 24) * days;
+
+        return Jwts.builder()
+                .header().add(headers).and()
+                .claims(payloads)
+                .issuedAt(Date.from(ZonedDateTime.now().toInstant()))
+                .expiration(Date.from(ZonedDateTime.now().plusMinutes(time).toInstant()))
+                .signWith(SignatureAlgorithm.HS256, key.getBytes(StandardCharsets.UTF_8))
+                .compact();
+    }
+
+    public Map<String, Object> validateToken(String token) throws JwtException {
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(key.getBytes(StandardCharsets.UTF_8)))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+}
