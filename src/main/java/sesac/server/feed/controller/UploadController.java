14703605a1c89431abe54c13e0c0f67d64c9c3b8
rtg <rtg1539@naver.com>
2024-09-18 17:06:11 +0900

    feat: upload controller

---
+++ src/main/java/sesac/server/feed/controller/UploadController.java
@@ -0,0 +1,78 @@
+package sesac.server.feed.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sesac.server.feed.dto.request.UploadRequest;
+import sesac.server.feed.dto.response.UploadResult;
+
+@Log4j2
+@RestController
+public class UploadController {
+
+    @Value("${upload-path}")
+    private String uploadPath;
+
+    @PostMapping(value = "upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public List<UploadResult> upload(UploadRequest request) {
+
+        log.info(request);
+
+        if (request.getFiles() != null) {
+
+            final List<UploadResult> list = new ArrayList<>();
+
+            request.getFiles().forEach(multipartFile -> {
+
+                String originalName = multipartFile.getOriginalFilename();
+                log.info(originalName);
+
+                String uuid = UUID.randomUUID().toString();
+
+                Path savePath = Paths.get(uploadPath, uuid + "_" + originalName);
+
+                boolean image = false;
+
+                try {
+                    multipartFile.transferTo(savePath);
+
+                    //이미지 파일의 종류라면
+                    if (Files.probeContentType(savePath).startsWith("image")) {
+
+                        image = true;
+
+                        File thumbFile = new File(uploadPath, "s_" + uuid + "_" + originalName);
+
+                        Thumbnailator
+                                .createThumbnail(savePath.toFile(), thumbFile, 200, 200);
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                list.add(UploadResult.builder()
+                        .uuid(uuid)
+                        .fileName(originalName)
+                        .img(image).build()
+                );
+
+            });//end each
+
+            return list;
+        }//end if
+
+        return null;
+    }
+}
