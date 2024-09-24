258f6ffbbb264d5cb77cc457a2f0b394947fc9f4
KIM SOOBIN <xpsxm225@naver.com>
2024-09-24 17:11:33 +0900

    fix: CampusController BindingHandler 수정

--- src/main/java/sesac/server/campus/controller/CampusController.java
+++ src/main/java/sesac/server/campus/controller/CampusController.java
@@ -44,7 +44,6 @@ public class CampusController {
 
     private final CampusService campusService;
     private final CourseService courseService;
-    private final BindingResultHandler bindingResultHandler;
 
     @GetMapping
     public ResponseEntity<List<CampusResponse>> getCampuses() {
@@ -166,32 +165,28 @@ public class CampusController {
     }
 
     private void validateCampusInput(BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            bindingResultHandler.handleBindingResult(bindingResult, List.of(
-                    CampusErrorCode.REQUIRED_NAME,
-                    CampusErrorCode.INVALID_NAME_SIZE,
-                    CampusErrorCode.REQUIRED_ADDRESS,
-                    CampusErrorCode.INVALID_ADDRESS_SIZE
-            ));
-        }
+        BindingResultHandler.handle(bindingResult, List.of(
+                CampusErrorCode.REQUIRED_NAME,
+                CampusErrorCode.INVALID_NAME_SIZE,
+                CampusErrorCode.REQUIRED_ADDRESS,
+                CampusErrorCode.INVALID_ADDRESS_SIZE
+        ));
     }
 
     private void validateCourseInput(BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            bindingResultHandler.handleBindingResult(bindingResult, List.of(
-                    CourseErrorCode.REQUIRED_NAME,
-                    CourseErrorCode.INVALID_NAME_SIZE,
+        BindingResultHandler.handle(bindingResult, List.of(
+                CourseErrorCode.REQUIRED_NAME,
+                CourseErrorCode.INVALID_NAME_SIZE,
 
-                    CourseErrorCode.REQUIRED_CLASS_NUMBER,
-                    CourseErrorCode.INVALID_CLASS_NUMBER_SIZE,
+                CourseErrorCode.REQUIRED_CLASS_NUMBER,
+                CourseErrorCode.INVALID_CLASS_NUMBER_SIZE,
 
-                    CourseErrorCode.REQUIRED_INSTRUCTOR_NAME,
-                    CourseErrorCode.INVALID_INSTRUCTOR_NAME_PATTERN,
-                    CourseErrorCode.INVALID_INSTRUCTOR_NAME_SIZE,
+                CourseErrorCode.REQUIRED_INSTRUCTOR_NAME,
+                CourseErrorCode.INVALID_INSTRUCTOR_NAME_PATTERN,
+                CourseErrorCode.INVALID_INSTRUCTOR_NAME_SIZE,
 
-                    CourseErrorCode.REQUIRED_START_DATE,
-                    CourseErrorCode.REQUIRED_END_DATE
-            ));
-        }
+                CourseErrorCode.REQUIRED_START_DATE,
+                CourseErrorCode.REQUIRED_END_DATE
+        ));
     }
 }
