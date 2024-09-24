3530c327148424bf1737a6ea9a40158dfeb9d4cb
KIM SOOBIN <xpsxm225@naver.com>
2024-09-24 16:59:50 +0900

    chore: Campus, Course 글자수 제한 변경

--- src/main/java/sesac/server/campus/dto/request/CreateCampusRequest.java
+++ src/main/java/sesac/server/campus/dto/request/CreateCampusRequest.java
@@ -5,11 +5,11 @@ import jakarta.validation.constraints.Size;
 
 public record CreateCampusRequest(
         @NotBlank(message = "REQUIRED_NAME")
-        @Size(min = 1, max = 50, message = "INVALID_NAME_SIZE")
+        @Size(min = 1, max = 20, message = "INVALID_NAME_SIZE")
         String name,
 
         @NotBlank(message = "REQUIRED_ADDRESS")
-        @Size(min = 1, max = 100, message = "INVALID_ADDRESS_SIZE")
+        @Size(min = 1, max = 150, message = "INVALID_ADDRESS_SIZE")
         String address
 ) {
 
