b7de99f759690a15f8c13d8e7a13c8ed1734d3cf
KIM SOOBIN <xpsxm225@naver.com>
2024-12-01 20:55:46 +0900

    test: noticeService 테스트 코드 수정

--- src/test/java/sesac/server/feed/service/NoticeServiceTest.java
+++ src/test/java/sesac/server/feed/service/NoticeServiceTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
+import java.util.List;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -35,11 +36,14 @@ class NoticeServiceTest {
     Student student;
     Manager manager;
 
+    Campus campus;
+    Course course;
+
     @BeforeEach
     public void setUp() {
         Fixture.em = em;
-        Campus campus = Fixture.createCampus("영등포");
-        Course course = Fixture.createCourse("영등포 자바", campus);
+        campus = Fixture.createCampus("영등포");
+        course = Fixture.createCourse("영등포 자바", campus);
 
         student = Fixture.createStudent("student1", course, 10);
         manager = Fixture.createManager("manager1", campus);
@@ -52,7 +56,7 @@ class NoticeServiceTest {
     public void create() {
         // give
         CreateNoticeRequest request =
-                new CreateNoticeRequest("제목", "내용", "", null, null);
+                new CreateNoticeRequest("제목", "내용", "", null, null, course.getId());
 
         // when
         Notice created = noticeService.createNotice(manager.getId(), request, NoticeType.ALL);
@@ -80,12 +84,12 @@ class NoticeServiceTest {
         em.clear();
 
         // when
-        NoticeResponse notice = noticeService.getNotice(created.getId());
+        NoticeResponse notice = noticeService.getNotice(manager.getUser().getId(), created.getId());
 
         // then
         assertThat(notice.title()).isEqualTo("제목");
         assertThat(notice.content()).isEqualTo("내용");
-        assertThat(notice.writer()).isEqualTo(manager.getCampus().getName());
+        assertThat(notice.campusName()).isEqualTo(manager.getCampus().getName());
     }
 
     // TODO 테스트 코드 수정
@@ -106,5 +110,30 @@ class NoticeServiceTest {
         em.clear();
 
         noticeService.getImportantNotices();
+    }
+
+    @Test
+    @DisplayName("중복 해시코드")
+    public void hashcodeTest() {
+        // give
+        CreateNoticeRequest request1 = new CreateNoticeRequest("제목", "내용", null,
+                List.of("해시1", "해시2"), 1, course.getId());
+        CreateNoticeRequest request2 = new CreateNoticeRequest("제목", "내용", null,
+                List.of("해시1", "해시2", "해시3"),
+                1, course.getId());
+
+        // when
+        Notice created1 = noticeService.createNotice(manager.getId(), request1, NoticeType.ALL);
+        Notice created2 = noticeService.createNotice(manager.getId(), request2, NoticeType.ALL);
+
+        em.flush();
+        em.clear();
+
+        // then
+        Notice notice1 = em.find(Notice.class, created1.getId());
+        Notice notice2 = em.find(Notice.class, created2.getId());
+
+        assertThat(notice1.getHashtags()).hasSize(2);
+        assertThat(notice2.getHashtags()).hasSize(3);
     }
 }
